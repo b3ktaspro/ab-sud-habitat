abbd78443db2e37195f8cc034a9801a9080a2fc0
b3ktas <b3ktas.pro@gmail.com>
2026-09-21 00:49:08 +0200

    update about page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,15 @@
 import CompanySection from "./About/CompanySection";
 import Intervention from "./About/Intervention";
+import Faq from "./About/Faq";
+import Services from "./About/Services";
 
 export default function About() {
   return (
     <>
       <CompanySection />
       <Intervention />
+      <Services />
+      <Faq />
     </>
   );
 }
